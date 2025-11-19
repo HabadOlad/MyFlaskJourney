6e7a053e15d6c73609f3c09cc0e89d15a68f75ff
Habad Olad <olad.hab@gmail.com>
2025-11-19 12:52:20 +0000

    feat:fixed wrong comments

--- mywebsite/app.py
+++ mywebsite/app.py
@@ -9,28 +9,28 @@ def indexpage():
 
 @app.route("/portfolio") #THis will send them to the first thing, backslash is a default which normally sends people to the index
 def portfoliopage():
-    return render_template("portfolio.html")  #this will send them to the index page
+    return render_template("portfolio.html")  #this will send them to the portfolio page
 
 @app.route("/projects") #THis will send them to the first thing, backslash is a default which normally sends people to the index
 def projectspage():
-    return render_template("projects.html")  #this will send them to the index page
+    return render_template("projects.html")  #this will send them to the projects page
 
 @app.route("/blog") #THis will send them to the first thing, backslash is a default which normally sends people to the index
 def blogpage():
-    return render_template("blog.html")  #this will send them to the index page
+    return render_template("blog.html")  #this will send them to the blog page
 
 @app.route("/cv") #THis will send them to the first thing, backslash is a default which normally sends people to the index
 def cvpage():
-    return render_template("cv.html")  #this will send them to the index page
+    return render_template("cv.html")  #this will send them to the cv page
 
 @app.route("/contactpage") #THis will send them to the first thing, backslash is a default which normally sends people to the index
 def contactpage():
-    return render_template("contactpage.html")  #this will send them to the index page
+    return render_template("contactpage.html")  #this will send them to the contactpage
 
 
 @app.route("/zork") #THis will send them to the first thing, backslash is a default which normally sends people to the index
 def zorkpage():
-    return render_template("zork.html")  #this will send them to the index page
+    return render_template("zork.html")  #this will send them to the zork game
 
 #This is should allow me to update python - Automatic Restarts (Reloader): When you save a change to a Python file (.py), the server automatically detects the change and reloads itself. This is what stops you from having to constantly stop and start Flask manually.
 if __name__ == "__main__":
