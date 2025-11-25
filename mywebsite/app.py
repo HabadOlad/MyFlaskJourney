11c7d53f415e4164c328bce14b4c0a1bc5477287
Habad Olad <olad.hab@gmail.com>
2025-11-25 04:56:41 +0000

    feat:slihgt updates

--- mywebsite/app.py
+++ mywebsite/app.py
@@ -7,28 +7,28 @@ def indexpage():
     return render_template("index.html")  #this will send them to the index page
 
 
-@app.route("/portfolio") #THis will send them to the first thing, backslash is a default which normally sends people to the index
+@app.route("/portfolio") #THis will send them to the first thing, backslash is a default which normally sends people to the portfolio
 def portfoliopage():
     return render_template("portfolio.html")  #this will send them to the portfolio page
 
-@app.route("/projects") #THis will send them to the first thing, backslash is a default which normally sends people to the index
+@app.route("/projects") #THis will send them to the first thing, backslash is a default which normally sends people to the projects
 def projectspage():
     return render_template("projects.html")  #this will send them to the projects page
 
-@app.route("/blog") #THis will send them to the first thing, backslash is a default which normally sends people to the index
+@app.route("/blog") #THis will send them to the first thing, backslash is a default which normally sends people to the blog
 def blogpage():
     return render_template("blog.html")  #this will send them to the blog page
 
-@app.route("/cv") #THis will send them to the first thing, backslash is a default which normally sends people to the index
+@app.route("/cv") #THis will send them to the first thing, backslash is a default which normally sends people to the cvpage
 def cvpage():
     return render_template("cv.html")  #this will send them to the cv page
 
-@app.route("/contactpage") #THis will send them to the first thing, backslash is a default which normally sends people to the index
+@app.route("/contactpage") #THis will send them to the first thing, backslash is a default which normally sends people to the contactpage
 def contactpage():
     return render_template("contactpage.html")  #this will send them to the contactpage
 
 
-@app.route("/zork") #THis will send them to the first thing, backslash is a default which normally sends people to the index
+@app.route("/zork") #THis will send them to the first thing, backslash is a default which normally sends people to the zork
 def zorkpage():
     return render_template("zork.html")  #this will send them to the zork game
 
